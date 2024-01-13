de29ec90e76f691790d5a8c6917779a45837c4d2
JonathanDiz <jdiazolivares007@gmail.com>
2024-01-13 17:33:22 -0300

    refactor: add components

--- weekflit/src/screens/HomeScreen.js
+++ weekflit/src/screens/HomeScreen.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import COLORS from '../constants/Colors';
+import GenreCard from '../components/GenreCard';
+import ItemSeparator from '../components/ItemSeparator';
+
+const Genres = ["All", "Action", "Comedy", "Romance", "Horror", "Sci-Fi"]
 
 const HomeScreen = () => {
 
   return (
-    <ScrollView style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Home screen</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <StatusBar 
+        style="auto" 
+        translucent={false} 
+        backgroundColor={COLORS.BASIC_BACKGROUND}
+      />
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>Now Playing</Text>
+        <Text style={styles.headerSubTitle}>VIEW ALL</Text>
+      </View>
+      <View style={styles.genreListContainer}>
+        <FlatList 
+          data={Genres}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item}
+          ItemSeparatorComponent={() => <ItemSeparator width={20} />}
+          ListHeaderComponent={() => <ItemSeparator width={20} />}
+          ListFooterComponent={() => <ItemSeparator width={20} />}
+          renderItem={({ item }) => <GenreCard />}
+        />
+      </View>
     </ScrollView>
   );
 }
@@ -18,7 +42,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: COLORS.BASIC_BACKGROUND, 
   },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  headerTitle: {
+    fontSize: 20,
+  },
+  headerSubTitle: {
+    fontSize: 13,
+    color: COLORS.ACTIVE,
+  },
+  genreListContainer: {
+    paddingVertical: 10
+  }
 });
 
 export default HomeScreen;
