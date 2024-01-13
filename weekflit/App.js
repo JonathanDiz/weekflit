ac8b0bb6d3f967ddc0af771f667d6c16efae72b4
JonathanDiz <jdiazolivares007@gmail.com>
2024-01-13 15:48:01 -0300

    refactor: update dependencies release

--- weekflit/App.js
+++ weekflit/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import VideoPlayer from './app/VideoPlayer';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import HomeScreen from './src/screens/HomeScreen';
+import MovieScreen from './src/screens/MovieScreen';
 
-export default function App() {
-  const videoSource = 'https://pluto.tv/ce84f691-66af-4c06-a402-ad37294b608f';
+const Stack = createStackNavigator()
+
+export default () => {
 
   return (
-    <View style={styles.container}>
-      <VideoPlayer videoSource={videoSource} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="movie"
+          component={MovieScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
